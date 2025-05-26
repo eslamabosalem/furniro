31fbcc10540578ce712f51262e8fb72d4542cd3d
eslamabosalem <eslamabosalem22@gmail.com>
2025-05-26 03:31:07 +0300

    done

--- context/FurnitureContext.tsx
+++ context/FurnitureContext.tsx
@@ -1,14 +1,13 @@
 "use client";
-import { createContext, Dispatch, SetStateAction } from "react";
-import furniture from "@/lib/furnit
-ure.json";
-import { useState } from "react";
+
+import { createContext, Dispatch, SetStateAction, useState } from "react";
+import furniture from "@/lib/furniture.json";
 
 type ProductsProps = typeof furniture.products;
 
 export type ProductsContextType = {
   products: ProductsProps;
-  setProducts: React.Dispatch<React.SetStateAction<ProductsProps>>;
+  setProducts: Dispatch<SetStateAction<ProductsProps>>;
   productsPerPage: number;
   setProductsPerPage: Dispatch<SetStateAction<number>>;
   sortValue: string;
@@ -20,18 +19,9 @@ export const ProductsContext = createContext<ProductsContextType | null>(null);
 function FurnitureContext({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  // Products
   const [products, setProducts] = useState(furniture.products);
-
-  // Products Per Page (For Pagination & Filter)
   const [productsPerPage, setProductsPerPage] = useState(8);
-
-  // Sort Filter
   const [sortValue, setSortValue] = useState("");
-
-  if (!ProductsContext) {
-    return <div>There Is No Products..</div>;
-  }
 
   return (
     <ProductsContext.Provider
@@ -48,4 +38,5 @@ function FurnitureContext({
     </ProductsContext.Provider>
   );
 }
+
 export default FurnitureContext;
